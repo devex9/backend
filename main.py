3a0717d8eb06f011bcedfd664fcb8819bc1144f2
devex9 <ajaykumar@Ajays-MacBook-Air.local>
2025-02-26 01:04:12 +0530

    added origin

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
 from fetch_data import fetch_nse_data
 
+
+from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Replace "*" with ["http://localhost:3000"] for security
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def home():
